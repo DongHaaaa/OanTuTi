2d428c6b077bd48d4d0ee54ac71926989d2e5d7e
DongHaaaa <dongdaiha9@gmail.com>
2026-08-19 18:22:48 +0700

    Initial commit

---
+++ Assets/Play.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI txtResult;
+    public TextMeshProUGUI txtEnemyChoice;
+
+    private string[] choices = { "Búa", "Bao", "Kéo" };
+
+    public void PlayerChoose(int playerChoice)
+    {
+        // Thêm dòng này: Bắt buộc Unity bỏ chọn nút vừa bấm
+        EventSystem.current.SetSelectedGameObject(null);
+
+        // --- Phần code bên dưới giữ nguyên như cũ ---
+        int enemyChoice = Random.Range(0, 3);
+        txtEnemyChoice.text = "Máy chọn: " + choices[enemyChoice];
+
+        if (playerChoice == enemyChoice)
+        {
+            txtResult.text = "Kết quả: HÒA!";
+        }
+        else if ((playerChoice == 0 && enemyChoice == 2) ||
+                 (playerChoice == 1 && enemyChoice == 0) ||
+                 (playerChoice == 2 && enemyChoice == 1))
+        {
+            txtResult.text = "Kết quả: BẠN THẮNG!";
+        }
+        else
+        {
+            txtResult.text = "Kết quả: BẠN THUA!";
+        }
+    }
+}
